0ce93c69c64807a60550acad8ed8f6f122ffcef4
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-05-22 00:30:00 -0300

    add arquivo q define Pacotes pra instalação

---
+++ PacotesParaInstalacao.py
@@ -0,0 +1,45 @@
+from Categoria import Categoria
+from Pacote import Pacote
+
+
+terminal = Categoria('TERMINAL')
+terminal.add_pacote(Pacote('apt', 'tree', 'tree'))
+terminal.add_pacote(Pacote('apt', 'bat', 'bat'))
+
+web = Categoria('WEB')
+web.add_pacote(Pacote('deb', 'https://download5.operacdn.com/ftp/pub/opera/desktop/110.0.5130.23/linux/opera-stable_110.0.5130.23_amd64.deb', 'opera.deb'))
+web.add_pacote(Pacote('deb', 'https://mega.nz/linux/repo/xUbuntu_24.04/amd64/megasync-xUbuntu_24.04_amd64.deb', 'mega.nz deb'))
+web.add_pacote(Pacote('deb', 'https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb', 'anydesk deb'))
+
+notas_organizacao = Categoria('NOTAS/ORGANIZAÇÃO')
+notas_organizacao.add_pacote(Pacote('flatpak', 'flathub com.toolstack.Folio', 'Folio'))
+notas_organizacao.add_pacote(Pacote('flatpak', 'flathub md.obsidian.Obsidian', 'Obsidian'))
+notas_organizacao.add_pacote(Pacote('flatpak', 'flathub org.onlyoffice.desktopeditors', 'Onlyoffice'))
+notas_organizacao.add_pacote(Pacote('git', 'git@github.com:DeboraCristina/CronogramaSimpleGUI.git', 'Cronograma'))
+
+desenvolvimento = Categoria('DESENVOLVIMENTO')
+desenvolvimento.add_pacote(Pacote('flatpak', 'flathub com.jetbrains.IntelliJ-IDEA-Community', 'intellij'))
+desenvolvimento.add_pacote(Pacote('dowload', 'https://eclipse.c3sl.ufpr.br/oomph/epp/2024-03/R/eclipse-inst-jre-linux64.tar.gz', 'eclipse'))
+desenvolvimento.add_pacote(Pacote('flatpak', 'flathub com.jetbrains.DataGrip', 'datagrip'))
+desenvolvimento.add_pacote(Pacote('deb', 'https://vscode.download.prss.microsoft.com/dbazure/download/stable/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/code_1.89.1-1715060508_amd64.deb', 'vscode'))
+desenvolvimento.add_pacote(Pacote('flatpak', 'flathub com.jetbrains.PyCharm-Community', 'pycharm'))
+desenvolvimento.add_pacote(Pacote('apt', 'python3-tk', 'python tkinter'))
+desenvolvimento.add_pacote(Pacote('deb', 'https://download.virtualbox.org/virtualbox/7.0.18/virtualbox-7.0_7.0.18-162988~Ubuntu~jammy_amd64.deb', 'virtual box'))
+
+ferramentas = Categoria('FERRAMENTAS')
+ferramentas.add_pacote(Pacote('flatpak', 'flathub com.github.PintaProject.Pinta', 'pinta'))
+ferramentas.add_pacote(Pacote('flatpak', 'flathub com.usebottles.bottles', 'bottles'))
+ferramentas.add_pacote(Pacote('dowload', 'https://medibangpaint.com/static/installer/MediBangPaintPro/MediBangPaintProSetup-29.1-64bit.exe', 'medibang paint'))
+ferramentas.add_pacote(Pacote('apt', 'mousepad', 'mousepad'))
+ferramentas.add_pacote(Pacote('apt', 'tilix', 'tilix'))
+ferramentas.add_pacote(Pacote('apt', 'flameshot', 'Flameshot'))
+
+pacotes_base = Categoria('PACOTES_BASE')
+pacotes_base.add_pacote(Pacote('apt', 'git', 'git'))
+pacotes_base.add_pacote(Pacote('apt', 'zsh', 'zsh'))
+pacotes_base.add_pacote(Pacote('apt', 'manpages', 'manpages'))
+pacotes_base.add_pacote(Pacote('apt', 'man-db', 'man-db'))
+pacotes_base.add_pacote(Pacote('apt', 'flatpak', 'flatpak'))
+pacotes_base.add_pacote(Pacote('apt', 'vim', 'vim'))
+pacotes_base.add_pacote(Pacote('apt', 'flatpak', 'flatpak'))
+pacotes_base.add_pacote(Pacote('apt', 'python3-pip', 'python pip'))
