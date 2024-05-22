aeb64fa0085f921d64cec9cb8af4fddcb9274768
DeboraCristina <deboracristinaproficional1@gmail.com>
2024-05-22 05:10:38 -0300

    add confirmação de instalacao manual

---
+++ InstalacaoManual.py
@@ -0,0 +1,17 @@
+import Utils as u
+
+
+def main():
+    while True:
+        print('Confirma que foi realizada a instalação manual?')
+        op = input('Confirma [sim ou nao]: ').lower()
+        if op == 'sim':
+            u.executar_comando('echo -n "MANUAL" > $HOME/.tipo_instalacao.txt')
+            u.print_sucesso('Instalação de programas habilitada')
+        if op == 'sim' or op == 'nao':
+            break
+        else:
+            u.print_falha('Ação inváçida!')
+
+if __name__ == '__main__':
+    main()
